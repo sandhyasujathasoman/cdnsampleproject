b263e588a5be1ace1f1b2bc0ccb0ffe449109df8
sandhyasujathasoman <sandhyass47@gmail.com>
2019-02-25 11:40:59 +0530

    Update CdnsampleprojectApplication.java

--- src/main/java/com/cdnsample/CdnsampleprojectApplication.java
+++ src/main/java/com/cdnsample/CdnsampleprojectApplication.java
@@ -1,18 +1,10 @@
 package com.cdnsample;
 
-import javax.swing.JFrame;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CdnsampleprojectApplication extends JFrame{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1362231856975057757L;
-
+public class CdnsampleprojectApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(CdnsampleprojectApplication.class, args);
 	}
